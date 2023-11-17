5e04dff6b6a070ec2e9b61117f8b98e793a0b34d
Rebeca Araujo <104568778+ReisLimaaaa@users.noreply.github.com>
2023-11-17 15:05:26 +0000

    funcionou

---
+++ calculadora/App.js
@@ -0,0 +1,118 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {useState} from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import { Entypo } from '@expo/vector-icons'; 
+
+
+
+export default function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const buttons = ['AC', 'DEL', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 3, 2, 1, '+', 0, '.', '+/-', '=']  
+
+  const [currentNumber, setCurrentNumber] = useState("")
+  const [lastNumber, setLastNumber] = useState("")
+
+
+  function calculator(){
+    const splitNumbers = currentNumber.split(' ')
+    const fistNumber = parseFloat(splitNumbers[0])
+    const lastNumber = parseFloat(splitNumbers[2])
+    const operator = splitNumbers[1]
+
+    switch(operator){
+      case '+':
+        setCurrentNumber((fistNumber + lastNumber).toString())
+        return
+      case '-': 
+        setCurrentNumber((fistNumber - lastNumber).toString())
+        return
+      case '*':
+        setCurrentNumber((fistNumber * lastNumber).toString())
+        return
+      case '/': 
+        setCurrentNumber((fistNumber / lastNumber).toString())
+        return
+    }
+  }
+
+  function handleInput(buttonPressed){
+    console.log(buttonPressed)
+    if(buttonPressed === '+' | buttonPressed === "-" | buttonPressed === "*" | buttonPressed === "/" ){
+      setCurrentNumber(currentNumber + " " + buttonPressed + " ")
+      return
+    }
+    switch(buttonPressed){
+      case 'DEL':
+        setCurrentNumber(currentNumber.substring(0, (currentNumber.length -1)))
+        return
+      case 'AC':
+        setLastNumber("")
+        setCurrentNumber("")
+        return
+      case '=':
+        setLastNumber(currentNumber + " = ")
+        calculator()
+        return
+      case '+/-':
+        return
+    }
+
+    setCurrentNumber(currentNumber + buttonPressed)
+  }
+
+  const styles = StyleSheet.create({
+    results: {
+      width: '100%',
+      minHeight: 280,
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
+	  backgroundColor: '#f5f5f5'
+    },
+    resultText: {
+      color: 'white',
+      margin: 10,
+      fontSize: 40
+    },
+    buttons: {
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+    },
+    button: {
+      borderColor:'#c8d6e5',
+      borderWidth: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      minWidth: 80, 
+      minHeight: 0,
+      flex: 2,
+    },
+    textButton: {
+      color: '#7c7c7c',
+      fontSize: 20,
+    }, 
+  });
+
+  return (
+    <View>
+      <View style={styles.results}>
+        <Text style={styles.historyText}>{lastNumber}</Text>
+        <Text style={styles.resultText}>{currentNumber}</Text>
+      </View>
+      <View style={styles.buttons}>
+        {buttons.map((button) => 
+          button === '=' ?
+        <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, {backgroundColor: '#9DBC7B'}]}>
+          <Text style={[styles.textButton, {color: "white", fontSize: 30}]}>{button}</Text>
+        </TouchableOpacity>
+          :
+          <TouchableOpacity onPress={() => handleInput(button)} key={button} style={[styles.button, 
+          {backgroundColor: typeof(button) === 'number' ? darkMode === true ? '#303946' : '#fff': darkMode === true ? '#414853' : '#ededed'}]}>
+            <Text style={styles.textButton}>{button}</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
+}
+
